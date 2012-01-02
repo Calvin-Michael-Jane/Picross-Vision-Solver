9140104f3bf9b243ad9dd5461e2d0c5aedbb565b
Michael Bailey <mabstorm@yahoo.com>
2012-01-01 20:00:24 -0600

    added example uses of OpenCV for c++ after failing to get python bindings to work due to issues with OS X Lion. we could potentially stream together c++ and python using output files or shared buffers

---
+++ testopencv/face_track1/CMakeFiles/objectDetection2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/objectDetection2.dir/objectDetection2.o"
+  "objectDetection2.pdb"
+  "objectDetection2"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/objectDetection2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
