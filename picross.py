2c3024a7e0c152dfcae18c0f78b6909078303769
Michael Bailey <mabstorm@yahoo.com>
2012-01-02 00:47:45 -0600

    renamed DetectPuzzle, made a test.sh script that executes all files so that each testing main is run

--- picross.py
+++ picross.py
@@ -5,7 +5,7 @@
 
 import GetPicture
 import AntiSkew
-import DetectPuzzle
+import DetectGrid
 import FindNumbers
 import OCRNumbers
 import PicrossSolver
@@ -15,7 +15,7 @@ import DisplayPuzzle
 if __name__=='__main__':
     original_picture = GetPicture.picture()
     skewless_picture = AntiSkew.fix_skew(original_picture)
-    puzzlegrid = DetectPuzzle.detect(skewless_picture)
+    puzzlegrid = DetectGrid.detect(skewless_picture)
     number_positions = FindNumbers.find_numbers(puzzlegrid, skewless_picture)
     puzzle = OCRNumbers.ocr(number_positions, skewless_picture)
     solution = PicrossSolver.solve(puzzle)
---
+++ test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# script to run all of the testing mains
+# for the picross pieces
+echo 'running main picross solver'
+python2.7 picross.py
+echo 'running GetPicture'
+python2.7 GetPicture.py
+echo 'running AntiSkew'
+python2.7 AntiSkew.py
+echo 'running DetectGrid'
+python2.7 DetectGrid.py
+echo 'running FindNumbers'
+python2.7 FindNumbers.py
+echo 'running OCRNumbers'
+python2.7 OCRNumbers.py
+echo 'running PicrossSolver'
+python2.7 PicrossSolver.py
+echo 'running DisplayPuzzle'
+python2.7 DisplayPuzzle.py
