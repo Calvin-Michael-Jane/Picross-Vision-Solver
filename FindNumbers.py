37640aaa454b5e6af8ecb159cb334b89d9029083
Michael Bailey <mabstorm@yahoo.com>
2012-01-02 18:23:42 -0600

    fixed misnamed import in FindNumbers

--- FindNumbers.py
+++ FindNumbers.py
@@ -3,7 +3,7 @@
 # Return the picture for use in the Picross Solver
 # May return a constant picture or a web camera image
 import Image
-import DetectPuzzle
+import DetectGrid
 
 def find_numbers(puzzlegrid, skewless_picture):
     return [(0,0), (1,1)]# number positions
@@ -13,7 +13,7 @@ def find_numbers(puzzlegrid, skewless_picture):
 if __name__=='__main__':
     test_pic = './testpicture.jpg'
     image = Image.open(test_pic)
-    puzzlegrid = DetectPuzzle.detect(image)
+    puzzlegrid = DetectGrid.detect(image)
     nums = find_numbers(puzzlegrid, image)
     print nums
 
