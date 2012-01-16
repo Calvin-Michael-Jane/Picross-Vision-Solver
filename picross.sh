e7e592b70a1fc29b61f98004d0fbc93b0623ff01
Michael Bailey <mabstorm@yahoo.com>
2012-01-16 12:06:23 -0500

    adding new training data. add webcam video for picross.sh

--- picross.sh
+++ picross.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+./testopencv/webcam/webcam &
 echo "GET READY AND PRESS [ENTER]"
 read userinput
 echo -ne "3..."
@@ -7,8 +8,11 @@ echo -ne "2..."
 sleep 1
 echo -ne "1..."
 sleep 1
-imagesnap -> snapshot.jpg
+./imagesnap -> snapshot.jpg
+afplay /Applications/Utilities/Grab.app/Contents/Resources/OpenShutter.snd
+afplay /Applications/Utilities/Grab.app/Contents/Resources/CloseShutter.snd
 echo "GOT IT"
+kill $!
 python2.7 ReadPic.py
 open snapshot_e.jpg
 
